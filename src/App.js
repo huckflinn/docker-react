e07a615ab58e04ca9b83d64a867403c2f85a160a
Nicholas Flinn <huckflinn@gmail.com>
2023-10-05 14:32:06 -0400

    Changed text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tá mé ag caint!
         </a>
       </header>
     </div>
