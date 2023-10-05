1a32fb7c3acebc30672633c7bb5b6982a014688b
Nicholas Flinn <huckflinn@gmail.com>
2023-10-05 14:38:08 -0400

    Changed text after failed test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Tá mé ag caint!
+          I was changed
         </a>
       </header>
     </div>
