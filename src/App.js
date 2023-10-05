2f87e9fb00e86d57fe433e7a391e759335f17eaf
Nicholas Flinn <huckflinn@gmail.com>
2023-10-05 14:46:20 -0400

    Corrected broken link and changed text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This time I was changed.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed
+          Learn React
         </a>
       </header>
     </div>
